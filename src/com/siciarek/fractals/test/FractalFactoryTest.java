04a220ab6db6a84d61866ed3d11b6e8857c88268
siciarek <siciarek@gmail.com>
2012-11-15 13:21:35 +0100

    Adds test suite

---
+++ src/com/siciarek/fractals/test/FractalFactoryTest.java
@@ -0,0 +1,61 @@
+package com.siciarek.fractals.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.siciarek.fractals.common.*;
+import com.siciarek.fractals.test.mocks.TestCanvas;
+
+@RunWith(Parameterized.class)
+public class FractalFactoryTest {
+
+	Drawable canvas;
+	private String given;
+	
+	public FractalFactoryTest(String given) {
+        this.given = given;
+    }
+
+	@Parameters
+	public static Collection<String[]> dataProvider() {
+		return Arrays.asList(new String[][] {
+			{ "Chaos Game Triangle" },
+			{ "Cantor Set 1D" },
+			{ "Koch Curve" },
+			{ "Koch Quadratic Curve" },
+			{ "Sierpinski Triangle" },
+			{ "Sierpinski Carpet" }
+		});
+	}
+
+	@Test
+	public void testNotImplementedFractal() {
+		Fractal f = com.siciarek.fractals.FractalFactory.get("Dummy", canvas);
+		assertEquals("Dummy is not implemented yet", f.name);
+	}
+	
+	@Test
+	public void testImplementedFractals() {
+		Fractal f = com.siciarek.fractals.FractalFactory.get(given, canvas);
+		assertEquals(given, f.name);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		canvas = new TestCanvas();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+}
---
+++ src/com/siciarek/fractals/test/mocks/TestCanvas.java
@@ -0,0 +1,67 @@
+package com.siciarek.fractals.test.mocks;
+
+import com.siciarek.fractals.common.Drawable;
+
+public class TestCanvas implements Drawable {
+
+	@Override
+	public void init(String style) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateTitle(String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPoint(float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveTo(float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void lineTo(float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void finalize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
